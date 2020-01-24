c6ba129075ed3ffd24bc71ff2e5f0cf169153a5c
vadymlos <vadym.los@pdffiller.team>
2020-01-24 19:26:23 +0200

    HomeWork3

--- src/HW3/Group.java
+++ src/HW3/Group.java
@@ -1,5 +1,43 @@
 package HW3;
 
 public class Group {
-    
+    public  Student [] gr = new Student[10];
+    public  int count = 0;
+
+    public void addGr(Student student) {
+        try {
+            gr[count] = student;
+            count++;
+        }catch (Exception e){
+            System.out.println("Array error");
+        }
+    }
+
+    public void removeFromGroup(Student student) {
+        for (int i = 0; i < gr.length; i++) {
+            try {
+                if(gr[i].equals(student)) {
+                    gr[i] = null;
+                }
+            }catch (NullPointerException e){
+               // System.out.println("test");
+            }
+
+        }
+    }
+
+    public void findStud(String lastname) {
+        for (int i = 0; i < gr.length; i++) {
+            try {
+                if(gr[i].getSecondname().equals(lastname)) {
+                    gr[i].aboutHuman();
+                }
+            }catch (Exception e){
+
+            }
+
+        }
+    }
+
+
 }
--- src/HW3/Main.java
+++ src/HW3/Main.java
@@ -3,11 +3,30 @@ package HW3;
 public class Main {
     public static void main(String[] args) {
         Human john = new Human("Jack", "Smith", 35);
-        john.aboutHuman();
+      //  john.aboutHuman();
         Student f1 = new Student("Forula1", john);
-        f1.aboutHuman();
+      //  f1.aboutHuman();
         Student f2 = new Student("Rusya", "Mongol", 29, "Formula 2");
-        f2.aboutHuman();
+        Student f3 = new Student("Yan", "Ban", 29, "Formula 3");
+        Student f4 = new Student("Igor", "Kachok", 29, "Formula 4");
+       // f2.aboutHuman();
 
+        Group group = new Group();
+        group.addGr(f1);
+        group.addGr(f2);
+        group.addGr(f3);
+        group.addGr(f4);
+
+        group.removeFromGroup(f3);
+        group.findStud("Mongol");
+
+        for (int i = 0; i < group.gr.length; i++) {
+            try {
+                System.out.println(group.gr[i].getName());
+            }catch (Exception e){
+
+            }
+
+        }
     }
 }
